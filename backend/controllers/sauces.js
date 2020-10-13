e4cc9644a45bcd46930387c01f4b7801e1679cd5
Stéphane Chimy <stephane.akira@gmail.com>
2020-10-13 16:25:14 +0200

    Ajout du reset des likes après ajout d'une sauce

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -9,8 +9,18 @@ exports.createSauce = (req, res, next) => {
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
   sauce.save()
+  // ici on va modifier toutes les sauces pour reset les likes
+  .then(() => {
+    Sauce.updateMany(
+      {},
+      {$set: {likes: 0, dislikes: 0, usersLiked:[], usersDisliked:[]}},
+      {multi: true}
+    )
     .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
     .catch(error => res.status(400).json({ error }));
+  })
+
+    
 };
 
 exports.getOneSauce = (req, res, next) => {
